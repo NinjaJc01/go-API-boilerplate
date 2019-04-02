f01af04fb8ad8593e1c5d142cfa6dd691c0b7b4f
NinjaJc01 <james@cookiehome.co.uk>
2019-04-02 10:37:20 +0100

    Initial Code Upload

---
+++ main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	go startServer()
+}
+func startServer() {
+	portPtr := flag.Int("p", 8081, "Port number to run the server on")
+	flag.Parse()
+	port := *portPtr
+	mr := mux.NewRouter()
+	mr.NotFoundHandler = http.HandlerFunc(notFoundHandler)
+	apiRouter := mr.PathPrefix("/api").Subrouter()
+	//Setup a static router for HTML/CSS/JS
+	mr.PathPrefix("/client/").Handler(http.StripPrefix("/client/", http.FileServer(http.Dir("./resources"))))
+	//CRUD API routes
+	aRouter := apiRouter.PathPrefix("/something").Subrouter()
+	/*Play A Song   */ aRouter.HandleFunc("/{id}", reqHandler).Methods("POST")
+	/*List all songs*/ aRouter.HandleFunc("/list", reqHandler).Methods("GET")
+	log.Println("Listening for requests")
+	http.ListenAndServe(fmt.Sprintf(":%v", port), mr)
+}
+
+func reqHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(404)
+}
+func notFoundHandler(w http.ResponseWriter, r *http.Request) { //Handle 404s
+	w.WriteHeader(404)
+}
